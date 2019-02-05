e8458606ab13485754ae53c4a84bad074f9fbec5
Andrey Artemenko <35538134+Andrew61-20@users.noreply.github.com>
2019-02-06 01:50:22 +0200

    Delete NoteFilter.js

--- src/components/NoteFilter.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NoteFilter = ({ filter, onFilterChange,placeholder }) => (
-  <input type="text" value={filter} onChange={onFilterChange} placeholder={placeholder} autoFocus />
-);
-
-export default NoteFilter;
